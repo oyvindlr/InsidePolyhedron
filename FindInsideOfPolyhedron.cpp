549c8031d286d1ff53dc43624d59b38a5d5542dd
Øyvind Rørtveit <oyvindlr@gmail.com>
2020-09-24 16:27:29 +0200

    For every face, the function now finds an inverse matrix in advance of ray tracing. Not faster, as I had hoped.

--- FindInsideOfPolyhedron.cpp
+++ FindInsideOfPolyhedron.cpp
@@ -45,7 +45,7 @@ static void warnOnce(const char *msg, int warningNo)
 }
 
 
-static void findExtremeCoords(const nBy3By3Array& faces, nBy3Array &minCoords, nBy3Array &maxCoords)
+static void findBoundingBox(const nBy3By3Array& faces, nBy3Array &minCoords, nBy3Array &maxCoords)
 {
 	minCoords.resize(faces.size());
 	maxCoords.resize(faces.size());
@@ -76,107 +76,77 @@ static void findFacesInDim(DynamicArray<int> &facesIndex, const nBy3Array &minCo
 			facesIndex.insertLast_unsafe(i); //Actually safe because the array has a capacity of nFaces elements
 }
 
-
-static void selectFaces(nBy3By3Array &selectedFaces, const nBy3By3Array& faces, const DynamicArray<int> &facesIndex)
+static void findFacesInDim(DynamicArray<int>& facesIndex, const nBy3Array &minCoords, const nBy3Array &maxCoords, double value, int dim, const DynamicArray<int>& facesIndexOld)
 {
-	selectedFaces.clear();
-	for (int i = 0; i < facesIndex.size(); i++)
-		selectedFaces.push_back(faces[facesIndex[i]]);
-}
-
-static void selectCoords(nBy3Array& minCoordsDim, nBy3Array& maxCoordsDim, const nBy3Array& minCoords, 
-	const nBy3Array& maxCoords, const DynamicArray<int>& facesIndex)
-{
-	minCoordsDim.clear();
-	maxCoordsDim.clear();
-	for (int i = 0; i < facesIndex.size(); i++)
+	facesIndex.clear();
+	size_t nFaces = facesIndexOld.size();
+	for (int i = 0; i < nFaces; i++)
 	{
-		minCoordsDim.push_back(minCoords[facesIndex[i]]);
-		maxCoordsDim.push_back(maxCoords[facesIndex[i]]);
+		int k = facesIndexOld[i];
+		if (minCoords[k][dim] <= value && maxCoords[k][dim] >= value)
+			facesIndex.insertLast_unsafe(k); //Actually safe because the array has a capacity of nFaces elements
 	}
 }
 
 
-/******
-* Removed this part for now. It used to check if the matrix is singular, which means that the plane defined
-* by the triangle is parallel to the ray. However, this can mean either that the ray doesn't cross the triangle
-* (which is fine) or that the triangular face lies along the ray, in which case we cannot determine whether or
-* not there is a crossing. Since the two situations are difficult to discern, I have chosen for now to ignore the 
-* problem. In most cases, if there is a real problem, we will at some point end up with an odd number of crossings
-* for a single ray, and therefore get a warning.
-******/
-#if 0
-static const char *singularWarning = "The plane defined by one of the triangle faces is along the line used in ray tracing. "
-	"Try adding random noise to your vertex coordinates to avoid this problem.";
-
-
-/** Solve a 2 by 2 linear equation A*x = b by Gaussian elimination with partial pivoting.
-*   b is overwritten by the result x.
-*  \returns False if the matrix is (numerically) singular, true otherwise.
+/** @brief  Invert a 2 by 2 matrix by Gaussian elimination with partial pivoting
+ * @param[in] A Matrix to be inverted
+ * @param[out] Ainv Inverted matrix
 */
-static bool solve2by2(const double A[2][2], double b[2])
+static void invert2by2(const double A[2][2], double Ainv[2][2])
 {
+
 	if (abs(A[0][0]) > abs(A[1][0]))//Partial pivoting
 	{
 		double fac = A[1][0] / A[0][0];
-		double a22 = A[1][1] - A[0][1] * fac;		
-		b[1] = (b[1] - b[0] * fac) / a22;
-		b[0] = (b[0] - A[0][1] * b[1]) / A[0][0];
-		return abs(a22) > EPS && abs(A[0][0]) > EPS;
+		double a22i = 1.0/ (A[1][1] - A[0][1] * fac);
+		Ainv[1][0] = -fac * a22i;
+		Ainv[0][0] = (1.0 - A[0][1] * Ainv[1][0]) / A[0][0];
+		Ainv[1][1] = a22i;
+		Ainv[0][1] = - A[0][1] * a22i / A[0][0];
 	}
 	else
 	{
 		double fac = A[0][0] / A[1][0];
-		double a12 = A[0][1] - A[1][1] * fac;
-		double b1 = (b[0] - b[1] * fac) / a12;
-		b[0] = (b[1] - A[1][1] * b1) / A[1][0];
-		b[1] = b1;
-		return abs(a12) > EPS && abs(A[1][0]) > EPS;
+		double a12i = 1.0 / (A[0][1] - A[1][1] * fac);
+		Ainv[0][0] = -A[1][1] * a12i / A[1][0];
+		Ainv[1][0] = a12i;
+		Ainv[1][1] = -fac * a12i;
+		Ainv[0][1] = (1.0 - A[1][1] * Ainv[1][1]) / A[1][0];
 	}
 }
-#endif
 
-/** Solve a 2 by 2 linear equation A*x = b by Gaussian elimination with partial pivoting.
-*   b is overwritten by the result x.
-*/
-static void solve2by2(const double A[2][2], double b[2])
+
+static void multiply2by2(const double A[2][2], double b[2])
 {
-	if (abs(A[0][0]) > abs(A[1][0]))//Partial pivoting
-	{
-		double fac = A[1][0] / A[0][0];
-		double a22 = A[1][1] - A[0][1] * fac;
-		b[1] = (b[1] - b[0] * fac) / a22;
-		b[0] = (b[0] - A[0][1] * b[1]) / A[0][0];
-	}
-	else
-	{
-		double fac = A[0][0] / A[1][0];
-		double a12 = A[0][1] - A[1][1] * fac;
-		double b1 = (b[0] - b[1] * fac) / a12;
-		b[0] = (b[1] - A[1][1] * b1) / A[1][0];
-		b[1] = b1;
-	}
+	double b0 = A[0][0]*b[0] + A[0][1]*b[1];
+	b[1] = A[1][0] * b[0] + A[1][1] * b[1];
+	b[0] = b0;
 }
+
 
 /** Get all crossings of triangular faces by a line in a specific direction */
-static void getCrossings(vector<double> &crossings, const nBy3By3Array& faces, const double coords[2], const int dimOrder[3])
+static void getCrossings(vector<double> &crossings, const nBy3By3Array& faces, const double coords[2], const int dimOrder[3], 
+	const DynamicArray<int> &facesIndex, double faceMatrices[][2][2])
 {
 	double b[2];
 	double A[2][2];
+	double Ainv[2][2];
 	int dim2 = dimOrder[2];
-	size_t nFaces = faces.size();
+	size_t nFaces = facesIndex.size();
 	crossings.clear();
 
-	for (int i = 0; i < nFaces; i++)
+	for (int k = 0; k < nFaces; k++)
 	{
+		int i = facesIndex[k];
 		for (int dimNo = 0; dimNo < 2; dimNo++)
 		{
 			int dim = dimOrder[dimNo];
 			b[dimNo] = coords[dimNo] - faces[i][0][dim];
-			A[dimNo][0] = faces[i][1][dim] - faces[i][0][dim];
-			A[dimNo][1] = faces[i][2][dim] - faces[i][0][dim];
 		}
-		solve2by2(A, b);
+
+		multiply2by2(faceMatrices[i], b);
+
 		if (b[0] > 0 && b[1] > 0 && ((b[0] + b[1]) < 1))
 		{
 			//No pun intended
@@ -187,13 +157,27 @@ static void getCrossings(vector<double> &crossings, const nBy3By3Array& faces, c
 	sort(crossings.begin(), crossings.end());
 }
 
+static void computeFaceMatrices(const nBy3By3Array &faces, double matrices[][2][2], int dimOrder[3])
+{
+	double A[2][2];
+	for (int i = 0; i < faces.size(); i++)
+	{
+		for (int dimNo = 0; dimNo < 2; dimNo++)
+		{
+			int dim = dimOrder[dimNo];
+			A[dimNo][0] = faces[i][1][dim] - faces[i][0][dim];
+			A[dimNo][1] = faces[i][2][dim] - faces[i][0][dim];
+		}
+		invert2by2(A, matrices[i]);
+	}
+}
 
 static inline bool isOdd(size_t n)
 {
 	return (n % 2) == 1;
 }
 
-static void buildFaceMatrix(nBy3By3Array& faces, const double vertices[][3], const int faceIndices[][3], size_t nFaces)
+static void buildFaceArray(nBy3By3Array& faces, const double vertices[][3], const int faceIndices[][3], size_t nFaces)
 {
 	faces.resize(nFaces);
 	for (int i = 0; i < nFaces; i++)
@@ -253,7 +237,7 @@ a triangular face.
 void insidePolyhedron(bool inside[], const double vertices[][3], const int faceIndices[][3], size_t nFaces, const double x[], size_t nx, const double y[], size_t ny, const double z[], size_t nz)
 {
 	nBy3By3Array faces;
-	buildFaceMatrix(faces, vertices, faceIndices, nFaces);
+	buildFaceArray(faces, vertices, faceIndices, nFaces);
 	insidePolyhedron(inside, faces, x, nx, y, ny, z, nz);
 }
 
@@ -281,15 +265,16 @@ void insidePolyhedron(bool inside[], const nBy3By3Array& faces, const double x[]
 	nBy3Array minCoords;
 	nBy3Array maxCoords;	
 	vector<double> crossings;
-	nBy3Array minCoordsD2;
-	nBy3Array maxCoordsD2;
-	nBy3By3Array facesD2;
-	nBy3By3Array facesD1;
 	size_t dimSteps[3];
+
+	
+	double(*faceMatrices)[2][2] = new double[faces.size()][2][2];
+
+	DynamicArray<int> facesIndex{faces.size()};
+	DynamicArray<int> facesIndex2{faces.size()};
 
 	resetWarnings();
 
-	DynamicArray<int> facesIndex {faces.size()};
 
 	size_t dimSize[3] = {nx, ny, nz};
 	int dimOrder[3] = {0, 1, 2};
@@ -297,7 +282,10 @@ void insidePolyhedron(bool inside[], const nBy3By3Array& faces, const double x[]
 	selectDimensionsForFastestProcessing(dimOrder, dimSize, dimSteps);
 	const int dim0 = dimOrder[0], dim1 = dimOrder[1], dim2 = dimOrder[2];
 
-	findExtremeCoords(faces, minCoords, maxCoords);
+	computeFaceMatrices(faces, faceMatrices, dimOrder);
+	
+
+	findBoundingBox(faces, minCoords, maxCoords);
 
 	const double *gridCoords[] = {x, y, z};
 
@@ -306,16 +294,13 @@ void insidePolyhedron(bool inside[], const nBy3By3Array& faces, const double x[]
 		findFacesInDim(facesIndex, minCoords, maxCoords, gridCoords[dim0][i], dim0);
 		if (facesIndex.size() == 0)
 			continue;
-		selectFaces(facesD2, faces, facesIndex);
-		selectCoords(minCoordsD2, maxCoordsD2, minCoords, maxCoords, facesIndex);
 		for (int j = 0; j < dimSize[1]; j++)
 		{
 			const double coords[2] = {gridCoords[dim0][i], gridCoords[dim1][j]};
-			findFacesInDim(facesIndex, minCoordsD2, maxCoordsD2, coords[1], dim1);
-			if (facesIndex.size() == 0)
+			findFacesInDim(facesIndex2, minCoords, maxCoords, coords[1], dim1, facesIndex);
+			if (facesIndex2.size() == 0)
 				continue;
-			selectFaces(facesD1, facesD2, facesIndex);
-			getCrossings(crossings, facesD1, coords, dimOrder);
+			getCrossings(crossings, faces, coords, dimOrder, facesIndex2, faceMatrices);
 			size_t nCrossings = crossings.size();
 			if (nCrossings == 0)
 				continue;
@@ -334,6 +319,7 @@ void insidePolyhedron(bool inside[], const nBy3By3Array& faces, const double x[]
 			}
 		}
 	}
+	delete[] faceMatrices;
 }
 
 
